036ab48d38fc58be2398d8b831e5265e8b453c67
José Antonio Vidal Sánchez <antoniovidal100@hotmail.com>
2016-01-23 21:21:29 -0600

    Primera Version de widget "Clock"

--- Visual/clock.cpp
+++ Visual/clock.cpp
@@ -1,14 +1,31 @@
 #include "clock.h"
 #include "ui_clock.h"
-
+#include <QDebug>
 Clock::Clock(QWidget *parent) :
-    QWidget(parent),
+    QLabel(parent),value(0),
     ui(new Ui::Clock)
 {
     ui->setupUi(this);
+    timer=new QTimer(this);
+    setText(QString::number(0));
+    repaint();
+    connect(timer,SIGNAL(timeout()),this,SLOT(update()));
+}
+
+void Clock::start(unsigned ms){
+    setText(QString::number(value));
+    timer->start(ms);
 }
 
 Clock::~Clock()
 {
     delete ui;
+    delete timer;
+}
+
+void Clock::update()
+{
+    qDebug()<<value;
+    setText(QString::number(++value));
+    emit updated(value);
 }
--- Visual/clock.h
+++ Visual/clock.h
@@ -1,22 +1,31 @@
 #ifndef CLOCK_H
 #define CLOCK_H
 
-#include <QWidget>
-
+#include <QLabel>
+#include <QTimer>
 namespace Ui {
 class Clock;
 }
 
-class Clock : public QWidget
+class Clock : public QLabel
 {
     Q_OBJECT
 
 public:
     explicit Clock(QWidget *parent = 0);
+    void start(unsigned ms);
+
     ~Clock();
 
 private:
+    QTimer* timer;
+    unsigned value;
     Ui::Clock *ui;
+signals:
+    void updated(unsigned value);
+public slots:
+    void update();
+
 };
 
 #endif // CLOCK_H
